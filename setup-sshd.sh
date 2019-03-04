6d781dd37025f1dd012f5cf6c5811c9926053670
daizhirui <daizhirui@hotmail.com>
2019-03-04 10:57:33 +0800

    add script for sshd setup

---
+++ setup-sshd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo sed "s/#Port\ 22/Port\ 8022/" /etc/ssh/sshd_config > new_sshd_config
+sudo cat new_sshd_config > /etc/ssh/ssh_config
+
+sudo sed "s/#PasswordAuthentication\ yes/PasswordAuthentication\ no/" > new_sshd_config
+sudo cat new_sshd_config > /etc/ssh/sshd_config
+
+sudo /etc/init.d/ssh restart
