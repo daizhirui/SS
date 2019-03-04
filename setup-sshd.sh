59b4c3862157276e65c5396c7614842aadee99f8
Mobile User <mobile@astros-iphone-8.local>
2019-03-04 12:14:37 +0800

    correct a mistake

--- setup-sshd.sh
+++ setup-sshd.sh
@@ -2,7 +2,7 @@
 
 echo 'Change port ... '
 sudo sed  's/#Port\ 22/Port\ 8022/' /etc/ssh/sshd_config > new_sshd_config
-sudo mv new_sshd_config /etc/ssh/ssh_config
+sudo mv new_sshd_config /etc/ssh/sshd_config
 
 echo 'Disable PasswordAuthentication ... '
 sudo sed  's/#PasswordAuthentication\ yes/PasswordAuthentication\ no/' /etc/ssh/sshd_config > new_sshd_config
