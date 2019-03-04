0f83d41ab3b3d0dba0c0814eea2dd628ce6c147c
daizhirui <daizhirui@hotmail.com>
2019-03-04 11:16:07 +0800

    no message

--- setup-sshd.sh
+++ setup-sshd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Change port ... "
-sudo sed "s/#Port\ 22/Port\ 8022/" /etc/ssh/sshd_config > new_sshd_config
+echo 'Change port ... '
+sudo sed -e 's/#Port\ 22/Port\ 8022/' /etc/ssh/sshd_config &> new_sshd_config
 sudo mv new_sshd_config /etc/ssh/ssh_config
 
-echo "Disable PasswordAuthentication ... "
-sudo sed "s/#PasswordAuthentication\ yes/PasswordAuthentication\ no/" > new_sshd_config
+echo 'Disable PasswordAuthentication ... '
+sudo sed -e 's/#PasswordAuthentication\ yes/PasswordAuthentication\ no/' > new_sshd_config
 sudo mv new_sshd_config /etc/ssh/sshd_config
 
 sudo /etc/init.d/ssh restart
