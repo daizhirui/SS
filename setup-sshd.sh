28fde62a8a05a93e0409680de41b6094c23d1a9e
daizhirui <daizhirui@hotmail.com>
2019-03-04 11:11:07 +0800

    change cat to mv

--- setup-sshd.sh
+++ setup-sshd.sh
@@ -2,10 +2,10 @@
 
 echo "Change port ... "
 sudo sed "s/#Port\ 22/Port\ 8022/" /etc/ssh/sshd_config > new_sshd_config
-sudo cat new_sshd_config > /etc/ssh/ssh_config
+sudo mv new_sshd_config /etc/ssh/ssh_config
 
 echo "Disable PasswordAuthentication ... "
 sudo sed "s/#PasswordAuthentication\ yes/PasswordAuthentication\ no/" > new_sshd_config
-sudo cat new_sshd_config > /etc/ssh/sshd_config
+sudo mv new_sshd_config /etc/ssh/sshd_config
 
 sudo /etc/init.d/ssh restart
